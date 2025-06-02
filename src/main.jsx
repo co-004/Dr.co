36e59e2e1e0cb61da1dbd031e888250d7a576d85
co-004 <cocolin40401234@gmail.com>
2025-06-02 17:11:12 +0800

    0602-0

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import '../scss/style.scss';
 // import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
